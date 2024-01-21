5217e9257561230c9603f451409aba140b882601
Mayuresh Karekar <mkarekar95@gmail.com>
2024-01-22 01:11:14 +0530

    1. Added toolbar.

--- app/src/main/java/com/matchmate/screen/MainActivity.kt
+++ app/src/main/java/com/matchmate/screen/MainActivity.kt
@@ -5,23 +5,49 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.matchmate.R
 import com.matchmate.ui.theme.MatchMateTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MatchMateTheme {
-                UserScreen()
+                Scaffold(topBar = {
+                    TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) })
+                }) {
+                    Box(modifier = Modifier.padding(it)) {
+                        App()
+                    }
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "users") {
+        composable(route = "users") {
+            UserScreen()
         }
     }
 }
